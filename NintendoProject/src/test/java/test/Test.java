31b78e5705c3b5b9799126af4458440151ba790b
AttaouileDev <h.attaouile@gmail.com>
2022-06-24 12:52:40 +0200

    Test jeu

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,10 +1,29 @@
 package test;
 
+import metier.Console;
+import metier.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Console c1 = new Console("PS5");
+		
+		Jeu j1= new Jeu("prince1", c1);
+		Jeu j2= new Jeu("prince2", c1);
+		Jeu j3= new Jeu("prince3", c1);
+		Jeu j4= new Jeu("prince4", c1);
+		Jeu j5= new Jeu("prince5", c1);
+		
+		
+
+
+		
+		
+		
+		
+		
 	}
 
 }
