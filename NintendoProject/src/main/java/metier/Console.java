42eb314e806784937da35b7f7fcce3428ae31973
Caroline <caroline.lay35@gmail.com>
2022-06-24 14:42:53 +0200

    Ajout attributs console

--- NintendoProject/src/main/java/metier/Console.java
+++ NintendoProject/src/main/java/metier/Console.java
@@ -1,11 +1,17 @@
 package metier;
 
+import java.time.LocalDate;
+
 public class Console {
 
 	private String nom;
-
-	public Console(String nom) {
+	private int prix;
+	private LocalDate dateSortie;
+	
+	public Console(String nom, int prix, LocalDate dateSortie) {
 		this.nom = nom;
+		this.prix = prix;
+		this.dateSortie = dateSortie;
 	}
 
 	public String getNom() {
@@ -16,10 +22,27 @@ public class Console {
 		this.nom = nom;
 	}
 
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public LocalDate getDateSortie() {
+		return dateSortie;
+	}
+
+	public void setDateSortie(LocalDate dateSortie) {
+		this.dateSortie = dateSortie;
+	}
+
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", dateSortie=" + dateSortie + "]";
 	}
-	
+
+
 	
 }
